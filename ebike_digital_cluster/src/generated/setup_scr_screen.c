826df011323c0e93f5e3ac8b6728bfb57faf6444
Chekhov Ma <chekhov.ma@nxp.com>
2024-03-11 14:54:11 +0800

    update license for gui-guider generated code
    Signed-off-by: Chekhov Ma <chekhov.ma@nxp.com>

--- ebike_digital_cluster/src/generated/setup_scr_screen.c
+++ ebike_digital_cluster/src/generated/setup_scr_screen.c
@@ -1,6 +1,6 @@
 /*
 * Copyright 2023 NXP
-* NXP Confidential and Proprietary. This software is owned or controlled by NXP and may only be used strictly in
+* NXP Proprietary. This software is owned or controlled by NXP and may only be used strictly in
 * accordance with the applicable license terms. By expressly accepting such terms or by downloading, installing,
 * activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to
 * comply with and are bound by, such license terms.  If you do not agree to be bound by the applicable license
@@ -578,5 +578,5 @@ void setup_scr_screen(lv_ui *ui)
 	//Update current screen layout.
 	lv_obj_update_layout(ui->screen);
 
-	
+
 }
